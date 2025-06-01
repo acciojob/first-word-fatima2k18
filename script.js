4553dcd4adc9552b22d8844c7af192ca1bc81d25
Fatima Ejaz <92662988+fatima2k18@users.noreply.github.com>
2025-06-01 16:46:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function firstWord(s) {
   // your code here
+	
+let firstWord = str.split(" ")[0];
+console.log(firstWord); // Output: "Apna"
 }
 
 // Do not change the code below
 
 const s = prompt("Enter String:");
 alert(firstWord(s));
+
